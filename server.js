46320d42d29e66c1d4f3b7d270ef0958e0dc66c4
vinxstudio <vinxstudio@gmail.com>
2020-02-14 00:04:07 +1100

    Updated Server file.

--- server.js
+++ server.js
@@ -1,24 +1,49 @@
 const express = require("express");
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
-
-app.use(function (req, res, next) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3001');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-  res.setHeader('Access-Control-Allow-Credentials', true);
+app.use(function(req, res, next) {
+  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3001");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
   next();
 });
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.post("/api/animal", (request, response) => {
-  //response.status(201).send('')
-  // response.status(404).send('{"code:404, "message":"Category \'master\' does not exist."}')
-  response
-    .status(422)
-    .send(
-      '{"code": 422, "message": "Validation failed.","errors": [{"field": "name","message": "This value should not be blank."}]}'
-    );
+ 
+  if (!request.body.name) {
+    return response
+      .status(422)
+      .send(
+        '{"code": 422, "message": "Validation failed.","errors": [{"field": "name","message": "This value should not be blank."}]}'
+      );
+  } else if (!request.body.category) {
+    return response
+      .status(404)
+      .send('{"code:404, "message":"Category \'master\' does not exist."}');
+  }
+
+  const insertAnimal = {
+   
+    name: request.body.name
+  };
+  //db.push(insertAnimal);
+  return response.status(201).send({
+    success: "true",
+    message: "added successfully",
+    insertAnimal
+  });
 });
 
 app.listen(port, err => {
